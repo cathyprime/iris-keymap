100051c27d4e7f798e50c32c0bf34c5c7a80a812
Cathyprime <magdajaroszynska47@gmail.com>
2023-06-29 22:10:45 +0200

    gaming mode + vim mode improvements
    more stuff for vim mode
    
    gaming mode
    
    and smaller compiled size

--- Yoolayn/keymap.c
+++ Yoolayn/keymap.c
@@ -4,13 +4,18 @@
 #define _SIGNS 1
 #define _VIM 2
 #define _MUSIC 3
+#define _GAMING 4
+
+bool visual_mode = false;
 
 enum custom_keycodes {
         QWERTY = SAFE_RANGE,
         SIGNS,
         VIM,
         MUSIC,
+        GAMING,
 };
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         [_QWERTY] = LAYOUT(
@@ -21,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
     SC_LSPO,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, SC_RSPC,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-    KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC,   TO(3),   KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+    KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC, QK_LEAD,   KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
 // └────────┴────────┴────────┴────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┴────────┴────────┴────────┘
                                         KC_LALT,   MO(1),  KC_SPC,   KC_ENT, KC_RALT, KC_LGUI
 //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
@@ -33,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    KC_LSFT, KC_LBRC,  KC_RBRC, KC_EQL, KC_MINS,  KC_QUOT,                   KC_QUOT, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_RSFT,
+    KC_LSFT, KC_LBRC, KC_RBRC,  KC_EQL, KC_MINS, KC_QUOT,                    KC_QUOT, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_RSFT,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-    KC_LCTL,  KC_NO,  KC_COMM, KC_DOT,  KC_BSLS, KC_SLSH,  KC_NO,   KC_TRNS, KC_SLSH, KC_BSLS, KC_COMM,  KC_DOT,  KC_NO,  KC_CAPS,
+    KC_LCTL,  KC_NO,  KC_COMM,  KC_DOT, KC_BSLS, KC_SLSH,  KC_NO,    TO(0),  KC_SLSH, KC_BSLS, KC_COMM,  KC_DOT,  KC_NO,  KC_CAPS,
 // └────────┴────────┴────────┴────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┴────────┴────────┴────────┘
                                          KC_LALT, KC_TRNS, KC_SPC,   KC_ESC,   MO(2), KC_LGUI
 //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
@@ -43,13 +48,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         [_VIM] = LAYOUT(
 // ┌────────┬────────┬──────────────┬────────┬────────┬─────────────┐                  ┌──────────┬────────┬────────┬────────┬──────────┬────────┐
-     KC_NO,   KC_NO,      KC_NO,      KC_END,  KC_NO,      KC_NO,                        KC_HOME,   KC_NO,   KC_NO,   KC_NO,  KC_HOME,    KC_NO,
+     KC_NO,   KC_NO,      KC_NO,      KC_END,  KC_NO,      KC_NO,                        KC_HOME,   KC_NO,   KC_NO,   KC_NO,   KC_HOME,   KC_NO,
 // ├────────┼────────┼──────────────┼────────┼────────┼─────────────┤                  ├──────────┼────────┼────────┼────────┼──────────┼────────┤
-     KC_NO,   KC_NO,  LCTL(KC_RGHT),  KC_END,  KC_NO,      KC_NO,                      LCTL(KC_C), KC_PGUP,  KC_NO,   KC_NO, LCTL(KC_V), KC_PPLS,
+     KC_NO,   KC_NO,  LCTL(KC_RGHT),  KC_END,  KC_NO,      KC_NO,                      LCTL(KC_C), KC_PGUP,  TO(0),   KC_NO, LCTL(KC_V), KC_PPLS,
 // ├────────┼────────┼──────────────┼────────┼────────┼─────────────┤                  ├──────────┼────────┼────────┼────────┼──────────┼────────┤
-     KC_NO,  KC_HOME,     KC_NO,     KC_PGDN,  KC_NO,      KC_NO,                        KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  KC_NO,    KC_NO,
+    KC_LSFT, KC_HOME,     KC_NO,     KC_PGDN,  KC_NO,      KC_NO,                        KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  KC_NO,   KC_RSFT,
 // ├────────┼────────┼──────────────┼────────┼────────┼─────────────┼────────┐┌────────┼──────────┼────────┼────────┼────────┼──────────┼────────┤
-     KC_NO,   KC_NO,     KC_BSPC,     KC_NO,   KC_NO,  LCTL(KC_LEFT), KC_NO,   KC_TRNS,  KC_DEL,   KC_BSPC,  KC_NO,   KC_NO,    KC_NO,    KC_NO,
+     KC_NO,   KC_NO,     KC_BSPC,     KC_NO,  KC_LSFT, LCTL(KC_LEFT), KC_NO,    TO(0),    KC_DEL,  KC_BSPC,  KC_NO,   KC_NO,    KC_NO,    KC_NO,
 // └────────┴────────┴──────────────┴────────┼────────┼─────────────┼────────┤├────────┼──────────┼────────┼────────┴────────┴──────────┴────────┘
                                                KC_NO,     KC_TRNS,    KC_NO,    KC_NO,   KC_TRNS,   KC_NO
 //                                           └────────┴─────────────┴────────┘└────────┴──────────┴────────┘
@@ -59,13 +64,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐                  ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    RGB_TOG, RGB_RMOD,RGB_MOD,  KC_NO,   KC_NO,   KC_NO,                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    RGB_TOG,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_NO,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,  KC_NO,                      KC_NO,  KC_PMNS, KC_PAST,  KC_NO,   KC_NO,   KC_NO,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
     KC_MUTE,  KC_NO,  KC_MPLY,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 // └────────┴────────┴────────┴────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┴────────┴────────┴────────┘
                                          KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO
+//                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
+         ),
+        [_GAMING] = LAYOUT(
+// ┌────────┬────────┬────────┬────────┬────────┬────────┐                  ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+// ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_QUOT,
+// ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
+    KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, SC_RSPC,
+// ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+    KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_BSPC,   TO(0),    KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+// └────────┴────────┴────────┴────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┴────────┴────────┴────────┘
+                                        KC_LCTL,  KC_SPC, KC_LALT,   KC_ENT, KC_RALT, KC_LGUI
 //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
         )
 };
@@ -107,19 +125,38 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 layer_state_t layer_state_set_user(layer_state_t state) {
         switch (get_highest_layer(state)) {
                 case 0:
+                        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_REACTIVE);
                         rgb_matrix_sethsv_noeeprom(210, 255, 200);
                         break;
                 case 1:
+                        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_REACTIVE);
                         rgb_matrix_sethsv_noeeprom(255, 255, 255);
                         break;
                 case 2:
+                        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_REACTIVE);
                         rgb_matrix_sethsv_noeeprom(170, 255, 255);
                         break;
                 case 3:
+                        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_REACTIVE);
                         rgb_matrix_sethsv_noeeprom(120, 175, 255);
+                        break;
+                case 4:
+                        rgb_matrix_mode_noeeprom(RGB_MATRIX_TYPING_HEATMAP);
                         break;
         }
         return state;
+}
+
+void leader_end_user(void) {
+        if (leader_sequence_one_key(KC_V)) {
+                layer_move(_VIM);
+        } else if (leader_sequence_one_key(KC_M)) {
+                layer_move(_MUSIC);
+        } else if (leader_sequence_one_key(KC_G)) {
+                layer_move(_GAMING);
+        } else if (leader_sequence_three_keys(KC_G, KC_C, KC_S)) {
+                SEND_STRING("git add -A\ngit commit -m ':skull:'\n");
+        }
 }
 
 void keyboard_post_init_user(void) {
@@ -144,6 +181,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         } else {
                                 layer_off(_SIGNS);
                                 update_tri_layer(_SIGNS, _VIM, _MUSIC);
+                                
                         }
                         return false;
                         break;
