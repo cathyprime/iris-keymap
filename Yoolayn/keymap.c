28752c9c10bdd8a36651733fd860e67a94c79448
Cathyprime <magdajaroszynska47@gmail.com>
2023-08-16 13:46:57 +0200

    feat(keymap): another super button

--- Yoolayn/keymap.c
+++ Yoolayn/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_QUOT,
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LSFT,C(KC_BSPC),KC_UP,  KC_BSPC,  KC_TAB,  KC_NO,                      KC_NO,  KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_LSFT,
+        KC_LSFT,C(KC_BSPC),KC_UP,  KC_BSPC,  KC_TAB, KC_LGUI,                     KC_NO,  KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_LSFT,
     // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LCTL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO,   KC_NO,  QK_LEAD,  QK_LEAD,  KC_DEL,  KC_NO,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
     // └────────┴────────┴────────┴────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┴────────┴────────┴────────┘
