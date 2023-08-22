1599498f28693acf40995ca13f9524ad70f07202
Cathyprime <magdajaroszynska47@gmail.com>
2023-08-22 22:45:13 +0200

    some drastic change
    make this keyboard more like regular qwerty one so it's easier to use
    them, also remove shifting row of numbers

--- Yoolayn/keymap.c
+++ Yoolayn/keymap.c
@@ -28,21 +28,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  QK_LEAD,  QK_LEAD,   KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
     // └────────┴────────┴────────┴────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┴────────┴────────┴────────┘
-                                            CW_TOGG,  OSL(1),  KC_SPC, KC_ENT,OSM(MOD_RALT),KC_LGUI
+                                            KC_LGUI,  MO(1),  KC_SPC,    KC_ENT,  KC_LALT, CW_TOGG
     //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
     ),
 
     [_SIGNS] = LAYOUT(
     // ┌────────┬────────┬────────┬────────┬────────┬────────┐                  ┌────────┬────────┬────────┬────────┬────────┬────────┐
-        KC_F1,    KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_QUOT,
+        KC_NO,    KC_GRV, KC_HOME,  KC_END,  KC_NO,   KC_NO,                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_QUOT,
     // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LSFT,C(KC_BSPC),KC_UP,  KC_BSPC,  KC_TAB, KC_LGUI,                     KC_NO,  KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_LSFT,
+        KC_LSFT,C(KC_BSPC),KC_NO,  KC_BSPC,  KC_TAB,  KC_NO,                     KC_LGUI, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_LSFT,
     // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LCTL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO,   KC_NO,  QK_LEAD,  QK_LEAD,  KC_DEL,  KC_NO,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+        KC_LCTL,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  QK_LEAD,  QK_LEAD,  KC_DEL,  KC_NO,  KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,
     // └────────┴────────┴────────┴────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┴────────┴────────┴────────┘
-                                            KC_CAPS, KC_TRNS, KC_TRNS,  KC_ESC,  KC_LALT, KC_LGUI
+                                            KC_LGUI, KC_TRNS, KC_TRNS,   KC_ESC, KC_RALT,  KC_CAPS
     //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
     ),
 
